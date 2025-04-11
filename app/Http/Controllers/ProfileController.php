5c10a7e9a9bbf7f8ea0adcd5f6bd1903fd7c069b
Oleksandr Kovalchuk <ol.kovalchuk.1989@gmail.com>
2025-04-11 15:50:10 +0300

    Extending profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,6 +8,11 @@ class ProfileController extends Controller
 {
     public function index()
     {
-        return view('profile.index');
+        $user = auth()->user();
+        $bmi = json_decode($user->bmi)[0] ?? null;
+        $ruffierIndex = json_decode($user->ruffierIndex)[0] ?? null;
+        $vitalSigns = json_decode($user->vitalSignsMeasurements)[0] ?? null;
+
+        return view('profile.index', compact('user', 'bmi', 'ruffierIndex', 'vitalSigns'));
     }
 }
--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -1,17 +1,18 @@
-@php
-$user = auth()->user();
-$bmi = json_decode($user->bmi) ?? null;
-$ruffierIndex = json_decode($user->ruffierIndex) ?? null;
-@endphp
 <x-main-layout>
-    <x-slot:title>{{ auth()->user()->first_name }} Profile</x-slot:title>
+    <x-slot:title>{{ auth()->user()->username }} Profile</x-slot:title>
     <h1 class="text-center text-3xl">{{ $user->username }} details</h1>
     <ul>
+        <!-- Basic user information -->
+        <h2 class="text-xl mb-1">Basic information</h2>
         <li><b>Full name: </b>{{ $user->first_name }} {{ $user->last_name }}</li>
         <li><b>Age: </b>{{ $user->age }} years</li>
-
+        <hr class="my-2">
+        <!-- Tests results and physical measurements -->
+        <h2 class="text-xl mb-1">Tests results and physical measurements</h2>
         @if($bmi)
-        <li><b>Body Mass Index: </b>{{ $bmi[0]->index }} ({{ $bmi[0]->result }})</li>
+        <li><b>Body Mass Index: </b>{{ $bmi->index }} ({{ $bmi->result }})</li>
+        <li><b>Height: </b>{{ $bmi->height }} m</li>
+        <li><b>Weight: </b>{{ $bmi->weight }} kg</li>
         @else
         <li>
         {!! '<a href="' .  route('health-tests.bmi') . '"class="bg-[#0275d8] text-white p-1 inline-block mt-2 rounded-md hover:bg-[#034680] hover:text-orange-300">
@@ -21,11 +22,26 @@ $ruffierIndex = json_decode($user->ruffierIndex) ?? null;
         @endif
 
         @if($ruffierIndex)
-        <li><b>Ruffier index: </b>{{ $ruffierIndex[0]->index }} ({{ $ruffierIndex[0]->result }})</li>
+        <li><b>Ruffier index: </b>{{ $ruffierIndex->index }} ({{ $ruffierIndex->result }})</li>
         @else
         <li>
         {!! '<a href="' .  route('health-tests.ruffier') . '"class="bg-[#5cb85c] text-white p-1 inline-block mt-2 rounded-md hover:bg-[#0c570c] hover:text-orange-300">
             Take Ruffier test
+        </a>' !!}
+        </li>
+        @endif
+        <hr class="my-2">
+        <!-- Vital signs -->
+        <h2 class="text-xl mb-1">Vital signs</h2>
+        @if ($vitalSigns)
+        <li><b>Temperature: </b>{{ $vitalSigns->temperature }} &deg;C</li>
+        <li><b>Heart rate: </b>{{ $vitalSigns->pulse }} bpm</li>
+        <li><b>Respiration rate: </b>{{ $vitalSigns->breathing }} rpm</li>
+        <li><b>Blood pressure: </b>{{ $vitalSigns->systolic }}/{{ $vitalSigns->diastolic }} mmHg</li>
+        @else
+        <li>
+        {!! '<a href="' .  route('vital-signs') . '"class="bg-[#20B2AA] text-white p-1 inline-block mt-2 rounded-md hover:bg-[#1C746F] hover:text-orange-300">
+            Fill vital signs
         </a>' !!}
         </li>
         @endif
